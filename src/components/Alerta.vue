0031b90d0744d089b7e4521c1666a73332446901
Enrique Castellanos <enriquecastellanos2403@gmail.com>
2024-09-21 20:05:39 -0600

    Subir proyecto, creación componente Presupuesto y Alerta

---
+++ src/components/Alerta.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class="alerta"
+    >
+        
+    </div>
+</template>
+
+<style scoped>
+    .alerta {
+        padding: 1rem;
+        font-weight: 900;
+        text-transform: uppercase;
+        font-size: 1.6rem;
+        text-align: center;
+        background-color: var(--blanco);
+        border-left: .5rem solid #B91C1C;
+    }
+</style>
